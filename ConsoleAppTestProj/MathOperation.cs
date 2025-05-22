275f171f2bb357485fcb637e453de6882a649be6
iamrazib <iamrazib@gmail.com>
2025-05-22 21:51:12 +0600

    adding new math class

---
+++ ConsoleAppTestProj/MathOperation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTestProj
+{
+    public class MathOperation
+    {
+        public int addition(int a, int b) {  return a + b; }
+        public int multi(int a, int b) {return a * b; }
+        public double divide(double a, double b) {
+            if (b == 0) throw new DivideByZeroException("Cannot divide by zero.");
+            return a / b;
+        }
+    }
+}
--- ConsoleAppTestProj/Program.cs
+++ ConsoleAppTestProj/Program.cs
@@ -1,8 +1,16 @@
-﻿internal class Program
+﻿using ConsoleAppTestProj;
+
+public class Program
 {
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
         Console.WriteLine("This is first line!");
+
+        MathOperation mp = new MathOperation();
+        Console.WriteLine("2+3="+ mp.addition(2, 3));
+        Console.WriteLine("4*6="+ mp.multi(4, 6));
+        Console.WriteLine("5/3="+ mp.divide(5, 3));
+        Console.ReadLine();
     }
 }
